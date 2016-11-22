65c9c283db647b57c2255838d0d180175b04cfe5
Shusil Dangi <shusil.dangi@kitware.com>
2016-11-21 19:10:15 -0500

    WarpImageFilter example and build added

---
+++ WarpImageFilter/build/CMakeFiles/WarpImageFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WarpImageFilter.dir/WarpImageFilter.cxx.o"
+  "WarpImageFilter.pdb"
+  "WarpImageFilter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WarpImageFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
